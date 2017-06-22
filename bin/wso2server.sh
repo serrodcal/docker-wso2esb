76373eb651bde589576ac4405a47c57c048add6f
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-22 14:26:16 +0200

    Fix wso2server.sh

--- bin/wso2server.sh
+++ bin/wso2server.sh
@@ -314,7 +314,7 @@ do
     -Dcom.ibm.cacheLocalHost=true \
     -DworkerNode=false \
     -DosgiConsole=19444 \
-    -Dfelix.felixinstall.dir=/opt/artifacts
+    -Dfelix.felixinstall.dir=/opt/artifacts \
     org.wso2.carbon.bootstrap.Bootstrap $*
     status=$?
 done
